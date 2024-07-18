495f96fe36203100bcd3d4e1fb9fe015467a4ce7
Sahil Yeole <sahilyeole93@gmail.com>
2024-07-18 16:46:44 +0530

    prepare for testing
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

---
+++ src/__tests__/Textinput.test.ts
@@ -0,0 +1,16 @@
+import React from 'react'
+import {render, fireEvent, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import TextInput from '../components/TextInput'
+
+test('renders input', () => {
+  //TODO
+})
+
+test('prints input', () => {
+  //TODO
+})
+
+test('sanitizes input', () => {
+  //TODO
+})
