50122a619785fe7a4b80787611d014e41fff42fe
swapnilpatil-github <swapnilpatil1012003@gmail.com>
2024-07-22 15:20:16 +0530

    Feat: Added DOMPurify configurations for TextInput component

--- src/__tests__/Textinput.test.tsx
+++ src/__tests__/Textinput.test.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import TextInput  from '../components/TextInput';
+import TextInput from '../components/TextInput';
 
 const handleChange = jest.fn();
-const Wrapper = () => {
+const Wrapper = ({ dompurifyConfig }: { dompurifyConfig?: { [key: string]: any } }) => {
   const [value, setValue] = useState('');
   const onChange = (newValue: string) => {
     setValue(newValue);
     handleChange(newValue);
   };
-  return <TextInput value={value} onChange={onChange} />;
+  return <TextInput value={value} onChange={onChange} dompurify={dompurifyConfig} />;
 };
 
 test('renders input', () => {
@@ -29,12 +29,55 @@ test('prints input', () => {
 });
 
 
+test('sanitizes input as per the dompurifyConfig', () => {
+  render(
+    <Wrapper
+      dompurifyConfig={{
+        ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'a'],
+        ALLOWED_ATTR: ['href'],
+        FORBID_TAGS: ['script'],
+        FORBID_ATTR: ['onclick'],
+        ALLOW_DATA_ATTR: false,
+        ALLOW_UNKNOWN_PROTOCOLS: false,
+        USE_PROFILES: {},
+        SANITIZE_DOM: false,
+        KEEP_CONTENT: false,
+        RETURN_TRUSTED_TYPE: false,
+      }}
+    />
+  );
+  const inputElement = screen.getByRole('textbox') as HTMLInputElement;
+
+  const testCases = [
+    { input: '<b>Bold</b>', expected: '<b>Bold</b>' },
+    { input: '<i>Italic</i>', expected: '<i>Italic</i>' },
+    { input: '<em>Emphasis</em>', expected: '<em>Emphasis</em>' },
+    { input: '<strong>Strong</strong>', expected: '<strong>Strong</strong>' },
+    { input: '<a href="http://example.com">Link</a>', expected: '<a href="http://example.com">Link</a>' },
+    { input: '<script>alert("XSS")</script>', expected: '' },
+    { input: '<div>Hello</div>', expected: 'Hello' },
+    { input: '<marquee>Hello world</marquee>', expected: 'Hello world' },
+  ];
+
+  testCases.forEach(({ input, expected }) => {
+    fireEvent.change(inputElement, { target: { value: input } });
+
+    expect(handleChange).toHaveBeenCalledWith(expected);
+
+    const sanitizedValue = inputElement.value;
+
+    expect(sanitizedValue).toBe(expected);
+  })
+
+});
+
+
 test('sanitizes various inputs with DOMPurify', () => {
   render(<Wrapper />);
   const inputElement = screen.getByRole('textbox');
 
   const testCases = [
-    {input:'<img src=x onerror=alert(1)//>',expected: '<img src="x">'},
+    { input: '<img src=x onerror=alert(1)//>', expected: '<img src="x">' },
     { input: '<script>alert("XSS")</script>', expected: '' },
     { input: '<a href="javascript:alert(1)">Click me</a>', expected: '<a>Click me</a>' },
     { input: '<div onclick="alert(1)">Hello</div>', expected: '<div>Hello</div>' },
--- src/components/BaseElement.tsx
+++ src/components/BaseElement.tsx
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DOMPurify from 'dompurify';
 
 interface BaseElementProps {
   tag: keyof JSX.IntrinsicElements;
   value: string;
   onChange: (value: string) => void;
+  dompurify?: {
+    ALLOWED_TAGS?: string[];
+    ALLOWED_ATTR?: string[];
+    FORBID_TAGS?: string[];
+    FORBID_ATTR?: string[];
+    ALLOW_DATA_ATTR?: boolean;
+    ALLOW_UNKNOWN_PROTOCOLS?: boolean;
+    USE_PROFILES?: { [profile: string]: boolean };
+    SANITIZE_DOM?: boolean;
+    KEEP_CONTENT?: boolean;
+    RETURN_TRUSTED_TYPE?: boolean;
+  };
   [key: string]: any;
 }
 
-const BaseElement: React.FC<BaseElementProps> = ({ tag: Tag, value, onChange, ...props }) => {
+const BaseElement: React.FC<BaseElementProps> = ({ tag: Tag, value, onChange, dompurify, ...props }) => {
   const handleChange = (event: React.ChangeEvent<any>) => {
-    const sanitizedValue = DOMPurify.sanitize(event.target.value);
+    console.log(dompurify)
+   
+    const sanitizedValue = DOMPurify.sanitize(event.target.value, dompurify || {});
+
     onChange(sanitizedValue);
+    
   };
 
   return <Tag {...props} value={value} onChange={handleChange} />;
