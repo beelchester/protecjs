9ac4e6be8dbc800fc20ba4d32382ced6e335e1dc
vvgcoder <gveda1054@gmail.com>
2024-07-27 21:40:53 +0530

    Add more dompurify configs (#12)
    * #k22
    
    Made changes in configs, where added new configs.
    
    * fix tests
    
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>
    
    * refact: Dompurify configs
    
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>
    
    ---------
    
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>
    Co-authored-by: Sahil Yeole <sahilyeole93@gmail.com>

--- src/components/BaseElement.tsx
+++ src/components/BaseElement.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import DOMPurify from 'dompurify';
+import DomPurifyConfig from './dompurifyConfig';
 
 interface BaseElementProps {
   tag: keyof JSX.IntrinsicElements;
   value: string;
   onChange: (value: string) => void;
-  dompurify?: {
-    ALLOWED_TAGS?: string[];
-    ALLOWED_ATTR?: string[];
-    FORBID_TAGS?: string[];
-    FORBID_ATTR?: string[];
-    ALLOW_DATA_ATTR?: boolean;
-    ALLOW_UNKNOWN_PROTOCOLS?: boolean;
-    USE_PROFILES?: { [profile: string]: boolean };
-    SANITIZE_DOM?: boolean;
-    KEEP_CONTENT?: boolean;
-    RETURN_TRUSTED_TYPE?: boolean;
-  };
+  dompurify?: DomPurifyConfig;
   [key: string]: any;
 }
 
 const BaseElement: React.FC<BaseElementProps> = ({ tag: Tag, value, onChange, dompurify, ...props }) => {
   const handleChange = (event: React.ChangeEvent<any>) => {
-    const sanitizedValue = DOMPurify.sanitize(event.target.value, dompurify || {});
+    const sanitized = DOMPurify.sanitize(event.target.value, dompurify || {});
+
+    const sanitizedValue = typeof sanitized === 'string' ? sanitized : sanitized.toString();
+
     onChange(sanitizedValue);
   };
 
