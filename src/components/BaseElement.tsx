aa686d1df2eb71c9f194c2255a8a0b223086b850
vvgcoder <gveda1054@gmail.com>
2024-07-27 21:14:21 +0530

    #k22
    Made changes in configs, where added new configs.

--- src/components/BaseElement.tsx
+++ src/components/BaseElement.tsx
@@ -16,13 +16,32 @@ interface BaseElementProps {
     SANITIZE_DOM?: boolean;
     KEEP_CONTENT?: boolean;
     RETURN_TRUSTED_TYPE?: boolean;
+    SAFE_FOR_JQUERY?: boolean;
+    SAFE_FOR_TEMPLATES?: boolean;
+    WHOLE_DOCUMENT?: boolean;
+    RETURN_DOM?: boolean;
+    RETURN_DOM_FRAGMENT?: boolean;
+    RETURN_DOM_IMPORT?: boolean;
+    IN_PLACE?: boolean;
+    ALLOW_ARIA_ATTR?: boolean;
+    ADD_TAGS?: string[];
+    ADD_ATTR?: string[];
+    FORBID_CONTENTS?: string[]; 
+    NAMESPACE?: string;
+    FORCE_BODY?: boolean;
+    FORBID_CSS?: boolean;
+    ALLOW_CSS_CLASSES?: string[];
+    SAFE_FOR_TWITTER?: boolean;
   };
   [key: string]: any;
 }
 
 const BaseElement: React.FC<BaseElementProps> = ({ tag: Tag, value, onChange, dompurify, ...props }) => {
   const handleChange = (event: React.ChangeEvent<any>) => {
-    const sanitizedValue = DOMPurify.sanitize(event.target.value, dompurify || {});
+    const sanitized = DOMPurify.sanitize(event.target.value, dompurify || {});
+
+    const sanitizedValue = typeof sanitized === 'string' ? sanitized : sanitized.toString();
+
     onChange(sanitizedValue);
   };
 
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -15,6 +15,22 @@ interface TextInputProps {
     SANITIZE_DOM?: boolean;
     KEEP_CONTENT?: boolean;
     RETURN_TRUSTED_TYPE?: boolean;
+    SAFE_FOR_JQUERY?: boolean;
+    SAFE_FOR_TEMPLATES?: boolean;
+    WHOLE_DOCUMENT?: boolean;
+    RETURN_DOM?: boolean;
+    RETURN_DOM_FRAGMENT?: boolean;
+    RETURN_DOM_IMPORT?: boolean;
+    IN_PLACE?: boolean;
+    ALLOW_ARIA_ATTR?: boolean;
+    ADD_TAGS?: string[];
+    ADD_ATTR?: string[];
+    FORBID_CONTENTS?: string[]; 
+    NAMESPACE?: string;
+    FORCE_BODY?: boolean;
+    FORBID_CSS?: boolean;
+    ALLOW_CSS_CLASSES?: string[];
+    SAFE_FOR_TWITTER?: boolean;
   };
   [key: string]: any;
 }
