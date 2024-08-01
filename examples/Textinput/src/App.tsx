9943d597eb7c1fbc7af1decff0120093e19eb582
Sujal99691 <sujalahirkar007@gmail.com>
2024-08-01 20:50:33 +0530

    Sql validation function

--- examples/Textinput/src/App.tsx
+++ examples/Textinput/src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 import './App.css';
 import { TextInput } from 'protecjs';
+import {validation}  from 'protecjs'; // <--- Import the validator function
 
 function App() {
   const [text, setText] = useState('');
+  const [isValid, setIsValid] = useState(true); // Add a state to track validation result
+
+  const handleInputChange = (input: string) => {
+    setText(input);
+    const validationResult = validation(input);
+    setIsValid(validationResult === undefined); // Set isValid to true if no error is thrown
+  };
+
   return (
     <div>
-      <TextInput value={text} onChange={setText}
+      <TextInput value={text} onChange={handleInputChange}
         dompurify={{
           ALLOWED_TAGS: ['i', 'em', 'strong', 'a'],
           ALLOWED_ATTR: ['href'],
@@ -15,8 +24,13 @@ function App() {
         }}
       />
       <p>Sanitized Text: {text}</p>
+      {isValid ? (
+        <p>Input is valid!</p>
+      ) : (
+        <p style={{ color: 'red' }}>Input is not valid!</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
