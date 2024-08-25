7fd42cefc69c6dcbcfa2e074ab29144374324b65
Sahil Yeole <sahilyeole93@gmail.com>
2024-08-25 14:03:35 +0530

    fix: sql validation and tests
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- src/__tests__/valildation.test.tsx
+++ src/__tests__/valildation.test.tsx
@@ -1,56 +1,25 @@
 import validation from '../functions/Validation';
 import fs from 'fs';
 import path from 'path';
-import { identify } from 'sql-query-identifier';
 
 interface TestCase {
   input: string;
   expected: string;
 }
 
-jest.mock('sql-query-identifier', () => ({
-  identify: jest.fn(),
-}));
-
-beforeAll(() => {
-  global.alert = jest.fn();
-});
-
 describe("SQL Validation Tests", () => {
-  const originalLog = console.log;
-
-  beforeEach(() => {
-    console.log = jest.fn();
-  });
-
-  afterEach(() => {
-    console.log = originalLog;
-    jest.restoreAllMocks();
-  });
-
   const testCasesPath = path.join(__dirname, 'sqlValidationTests.json');
   const testCases: TestCase[] = JSON.parse(fs.readFileSync(testCasesPath, 'utf8'));
 
   testCases.forEach(({ input, expected }) => {
     test(`Validates query: ${input}`, () => {
-      const expectedTypeMatch = expected.match(/SQL query of type (\w+)/);
-      const expectedType = expectedTypeMatch ? expectedTypeMatch[1] : 'UNKNOWN';
-
-      (identify as jest.Mock).mockReturnValue([{ type: expectedType }]);
-
+      let result: string | undefined;
       try {
-        validation(input);
-      } catch (error) {
-        if (error instanceof Error) {
-          expect(error.message).toBe(expected);
-        } else {
-          throw error;
-        }
+        validation(input, { sql: true });
+      } catch (error: any) {
+        result = error.message;
       }
-
-      if (expected.includes("SQL query detected!")) {
-        expect(global.alert).toHaveBeenCalledWith("SQL query detected!");
-      }
+      expect(result).toBe(expected);
     });
   });
 });
--- src/functions/Validation.tsx
+++ src/functions/Validation.tsx
@@ -1,67 +1,67 @@
 import { identify } from "sql-query-identifier";
 
 const keywords = [
+  "INSERT",
   "UPDATE",
   "DELETE",
   "SELECT",
   "TRUNCATE",
-  "CREATE_DATABASE",
-  "CREATE_SCHEMA",
-  "CREATE_TABLE",
-  "CREATE_VIEW",
-  "CREATE_TRIGGER",
-  "CREATE_FUNCTION",
-  "CREATE_INDEX",
-  "CREATE_PROCEDURE",
-  "DROP_DATABASE",
-  "DROP_SCHEMA",
-  "DROP_TABLE",
-  "DROP_VIEW",
-  "DROP_TRIGGER",
-  "DROP_FUNCTION",
-  "DROP_INDEX",
-  "DROP_PROCEDURE",
-  "ALTER_DATABASE",
-  "ALTER_SCHEMA",
-  "ALTER_TABLE",
-  "ALTER_VIEW",
-  "ALTER_TRIGGER",
-  "ALTER_FUNCTION",
-  "ALTER_INDEX",
-  "ALTER_PROCEDURE",
-  "ANON_BLOCK", // BigQuery and Oracle dialects only
-  "SHOW_BINARY", // MySQL and generic dialects only
-  "SHOW_BINLOG", // MySQL and generic dialects only
-  "SHOW_CHARACTER", // MySQL and generic dialects only
-  "SHOW_COLLATION", // MySQL and generic dialects only
-  "SHOW_COLUMNS", // MySQL and generic dialects only
-  "SHOW_CREATE", // MySQL and generic dialects only
-  "SHOW_DATABASES", // MySQL and generic dialects only
-  "SHOW_ENGINE", // MySQL and generic dialects only
-  "SHOW_ENGINES", // MySQL and generic dialects only
-  "SHOW_ERRORS", // MySQL and generic dialects only
-  "SHOW_EVENTS", // MySQL and generic dialects only
-  "SHOW_FUNCTION", // MySQL and generic dialects only
-  "SHOW_GRANTS", // MySQL and generic dialects only
-  "SHOW_INDEX", // MySQL and generic dialects only
-  "SHOW_MASTER", // MySQL and generic dialects only
-  "SHOW_OPEN", // MySQL and generic dialects only
-  "SHOW_PLUGINS", // MySQL and generic dialects only
-  "SHOW_PRIVILEGES", // MySQL and generic dialects only
-  "SHOW_PROCEDURE", // MySQL and generic dialects only
-  "SHOW_PROCESSLIST", // MySQL and generic dialects only
-  "SHOW_PROFILE", // MySQL and generic dialects only
-  "SHOW_PROFILES", // MySQL and generic dialects only
-  "SHOW_RELAYLOG", // MySQL and generic dialects only
-  "SHOW_REPLICAS", // MySQL and generic dialects only
-  "SHOW_SLAVE", // MySQL and generic dialects only
-  "SHOW_REPLICA", // MySQL and generic dialects only
-  "SHOW_STATUS", // MySQL and generic dialects only
-  "SHOW_TABLE", // MySQL and generic dialects only
-  "SHOW_TABLES", // MySQL and generic dialects only
-  "SHOW_TRIGGERS", // MySQL and generic dialects only
-  "SHOW_VARIABLES",
-  "SHOW_WARNINGS",
+  "CREATE DATABASE",
+  "CREATE SCHEMA",
+  "CREATE TABLE",
+  "CREATE VIEW",
+  "CREATE TRIGGER",
+  "CREATE FUNCTION",
+  "CREATE INDEX",
+  "CREATE PROCEDURE",
+  "DROP DATABASE",
+  "DROP SCHEMA",
+  "DROP TABLE",
+  "DROP VIEW",
+  "DROP TRIGGER",
+  "DROP FUNCTION",
+  "DROP INDEX",
+  "DROP PROCEDURE",
+  "ALTER DATABASE",
+  "ALTER SCHEMA",
+  "ALTER TABLE",
+  "ALTER VIEW",
+  "ALTER TRIGGER",
+  "ALTER FUNCTION",
+  "ALTER INDEX",
+  "ALTER PROCEDURE",
+  "SHOW BINARY", // MySQL and generic dialects only
+  "SHOW BINLOG", // MySQL and generic dialects only
+  "SHOW CHARACTER", // MySQL and generic dialects only
+  "SHOW COLLATION", // MySQL and generic dialects only
+  "SHOW COLUMNS", // MySQL and generic dialects only
+  "SHOW CREATE", // MySQL and generic dialects only
+  "SHOW DATABASES", // MySQL and generic dialects only
+  "SHOW ENGINE", // MySQL and generic dialects only
+  "SHOW ENGINES", // MySQL and generic dialects only
+  "SHOW ERRORS", // MySQL and generic dialects only
+  "SHOW EVENTS", // MySQL and generic dialects only
+  "SHOW FUNCTION", // MySQL and generic dialects only
+  "SHOW GRANTS", // MySQL and generic dialects only
+  "SHOW INDEX", // MySQL and generic dialects only
+  "SHOW MASTER", // MySQL and generic dialects only
+  "SHOW OPEN", // MySQL and generic dialects only
+  "SHOW PLUGINS", // MySQL and generic dialects only
+  "SHOW PRIVILEGES", // MySQL and generic dialects only
+  "SHOW PROCEDURE", // MySQL and generic dialects only
+  "SHOW PROCESSLIST", // MySQL and generic dialects only
+  "SHOW PROFILE", // MySQL and generic dialects only
+  "SHOW PROFILES", // MySQL and generic dialects only
+  "SHOW RELAYLOG", // MySQL and generic dialects only
+  "SHOW REPLICAS", // MySQL and generic dialects only
+  "SHOW SLAVE", // MySQL and generic dialects only
+  "SHOW REPLICA", // MySQL and generic dialects only
+  "SHOW STATUS", // MySQL and generic dialects only
+  "SHOW TABLE", // MySQL and generic dialects only
+  "SHOW TABLES", // MySQL and generic dialects only
+  "SHOW TRIGGERS", // MySQL and generic dialects only
+  "SHOW VARIABLES",
+  "SHOW WARNINGS",
   "UNKNOWN"
 ];
 
@@ -97,7 +97,6 @@ export default function validation(input: string, type: ValidationType = {}) {
   if (isSql) {
     const sqlQueries = extractSQLQueries(input);
     if (sqlQueries.length > 0) {
-      console.error("SQL query detected!");
       for (const query of sqlQueries) {
         const res = identify(query, { strict: false });
         if (res[0].type !== 'UNKNOWN') {
