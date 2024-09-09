2f543859f4e1721bd633e4ef5ec70c98d27c29f1
Aatif8459 <aatifkhan2605@gmail.com>
2024-09-09 21:21:26 +0530

    updated main.tsx

--- examples/Textinput/src/main.tsx
+++ examples/Textinput/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider, CSPMeta } from 'protecjs'; // Importing from your library
+import { HelmetProvider, CSPMeta } from 'protecjs';
 import App from './App.tsx';
 import './index.css';
 
